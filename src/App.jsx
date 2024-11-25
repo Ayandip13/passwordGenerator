7362e3f224b4b2fcc8fc2e29f2fce49e92fc8eba
ayandip paul <ayandippaul284@gmail.com>
2024-11-25 14:46:12 +0530

    inputField and copy added

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,45 @@
-import { useState, useCallback } from 'react'
-import './App.css'
+import { useState, useCallback } from "react";
 
 function App() {
-  const [length, setLength] = useState(8)
-  const [numberAllowed, setnumberAllowed] = useState(false)
-  const [charAllowed, setcharAllowed] = useState(false)
-  const [password, setPassword] = useState("")
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setnumberAllowed] = useState(false);
+  const [charAllowed, setcharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
 
-  const passwordGenerator = useCallback(()=>{
-    let pass = "" //we will add the generated passwords in this `pass` variable and using setPassword method we'll store the password into state variable `password`
-    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  const passwordGenerator = useCallback(() => {
+    let pass = ""; //we will add the generated passwords in this `pass` variable and using setPassword method we'll store the password into state variable `password`
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-    if (numberAllowed) str += "0123456789"
-    if (charAllowed) str += "!@#$%^&*()_+{}:~`/|<>.,[]"
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*()_+{}:~`/|<>.,[]";
 
     for (let i = 1; i <= length; i++) {
-      let char = Math.floor(Math.random()*str.length+1)
+      let char = Math.floor(Math.random() * str.length + 1);
+      let pass = str.charAt(char);
     }
 
-  }, [length, numberAllowed, charAllowed, setPassword])
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
 
   return (
     <>
-      <h1 className='text-sky-400 text-4xl text-center my-4'>Password</h1>
+      <div className="w-full mx-auto max-w-md shadow-md rounded-lg px-4 py-3 my-8 text-neutral-200 bg-gray-700">
+        <p className="text-white text-center my-2">Password generator</p>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+          />
+          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+            Copy
+          </button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
